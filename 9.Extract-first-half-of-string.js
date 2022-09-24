25bfca92cfa0649bae9bded454d1d025d15c466e
Jahid Bin Alam <jahidbinalam94@gmail.com>
2022-09-24 14:53:20 +0530

    8 challenge solved

---
+++ 9.Extract-first-half-of-string.js
@@ -0,0 +1,14 @@
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
+
+function
+myFunction
+(
+a
+)
+{
+let text_length = a.length
+let first_half = round(text_length/2)
+return a.slice(0, first_half)
+}
