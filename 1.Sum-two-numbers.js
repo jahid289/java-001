81773fe48449823afc86c56a86f2405d6cc6dc9d
Jahid Bin Alam <jahidbinalam94@gmail.com>
2022-09-24 13:18:37 +0530

    3rd commit

--- 1.Sum-two-numbers.js
+++ 1.Sum-two-numbers.js
@@ -1,1 +1,12 @@
-console.log("hello world!");
+
+
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+function
+myFunction(a, b){
+return a + b;
+}
+
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b and return the result
